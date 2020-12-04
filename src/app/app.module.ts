b90a9ca33ee32d0efb7eef50a34fbea6e62790ee
nidhal <nidhal.jelassi@fsegt.utm.tn>
2020-12-04 16:27:29 +0100

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZQpQS3GfTMAQYMp3WOHL5R5OanDJBtdw",
+  authDomain: "todo-gl-a.firebaseapp.com",
+  projectId: "todo-gl-a",
+  storageBucket: "todo-gl-a.appspot.com",
+  messagingSenderId: "754927319731",
+  appId: "1:754927319731:web:d625d5cd0e961c8579cec6"
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  currentDate : string;
+  newTask : string = '';
+  allTask = [];
 
-  constructor() {}
+  constructor(private angFire : AngularFireDatabase) {
+    let myDate = new Date();
+    let options = {weekday : 'long', month :'long', day : 'numeric' }
+    this.currentDate = myDate.toLocaleDateString('en-en', options)
+  }
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  addNewTask() {
+    this.angFire.list('Tasks/').push({
+      text : this.newTask,
+      date : new Date().toISOString(),
+      checked : false
+    });
+    this.newTask = '';
+
+  }
+
+  getTasks() {
+    this.angFire.list('Tasks/').snapshotChanges(['child_added']).subscribe(
+      (reponse) => {
+        console.log(reponse);
+        this.allTask = [];
+        reponse.forEach( element => {
+          //console.log('****', element)
+          this.allTask.push({
+            key : element.key,
+            text : element.payload.exportVal().text,
+            date : element.payload.exportVal().date.substring(11, 16),
+            checked : element.payload.exportVal().checked
+          })
+
+        })
+      }
+    )
+  }
+
+  changeCheckedState(tsk) {
+    this.angFire.object(`Tasks/${tsk.key}/checked/`).set(tsk.checked);
+  }
+
+  deleteTask(id) {
+    this.angFire.list('Tasks/').remove(id);
+    this.getTasks();
+  }
 
 }
